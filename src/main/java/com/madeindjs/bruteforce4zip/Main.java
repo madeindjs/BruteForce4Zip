52592114b58ee03b635e8a70a53e19a73a307d4b
Rousseau Alexandre <contact@rousseau-alexandre.fr>
2018-03-26 07:37:32 +0200

    Initial commit

---
+++ src/main/java/com/madeindjs/bruteforce4zip/Main.java
@@ -0,0 +1,9 @@
+package com.madeindjs.bruteforce4zip;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("com.madeindjs.bruteforce4zip.Main.main()");
+    }
+
+}
